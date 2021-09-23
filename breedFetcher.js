102267354d7eed331fc795eccd8df4678d051e8e
Nick <nsagias@gmail.com>
2021-09-23 18:26:23 -0400

    fixed bug and added testing

--- breedFetcher.js
+++ breedFetcher.js
@@ -4,23 +4,19 @@ const fetchBreedDescription = function(breedName, callback) {
   const req = `https://api.thecatapi.com/v1/breeds/search/?q=${breedName}`;
 
   request(req, (error,response, body) => {
+    
     if (error) {
       return callback(error);
-      // console.log(error);
-     
-    }
   
-    const data = JSON.parse(body);
-  
+    } 
+      const data = JSON.parse(body);
     if (data[0] === undefined) {
-      return callback(`requested breed named: '${breedName}' was not found`);
-      // console.log(`requested breed named: '${breed}' was not found`);
+      return callback(`requested breed named: ${breedName} was not found`);
       
-    }
-  
-    const catDescription = data[0].description;
-    callback(catDescription);
-    // console.log(catDescription);
+    } 
+      const catDescription = data[0].description;
+      return callback(null, catDescription);
+   
   });
   
 };
--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const { fetchBreedDescription } = require('./breedFetcher');
 const breedName = process.argv[2];
 
 
-fetchBreedDescription(breedName, (desc, error) => {
+fetchBreedDescription(breedName, (error, desc) => {
   if (error) {
     console.log('Error fetch details:', error);
   } else {
